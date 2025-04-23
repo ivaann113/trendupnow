848e93747ef4e6df9a12a246a703098903433334
ivaann113 <ivanucedaruiz@gmail.com>
2025-04-23 11:11:19 +0200

    Agrego root layout obligatorio para Next.js

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
 export default function RootLayout({
-    children,
-  }: {
-    children: React.ReactNode;
-  }) {
-    return (
-      <html lang="en">
-        <body>{children}</body>
-      </html>
-    );
-  }
-  
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head />
+      <body>{children}</body>
+    </html>
+  );
+}
